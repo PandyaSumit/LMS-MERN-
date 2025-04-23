ead4eaf9a892b55a9dc129d6bb25e1b04ae03ea1
Sumit Pandya <pandyasumit01@gmail.com>
2025-04-23 22:40:19 +0530

    appcontext changes

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -24,7 +24,7 @@ export const AppContextProvider = (props) => {
     // Fetch All Courses
     const fetchAllCourses = async () => {
         try {
-            const { data } = await axios.get(`${backendUrl}/api/course/al`);
+            const { data } = await axios.get(`${backendUrl}/api/course/all`);
             if (data.success) {
                 setAllCourses(data.courses)
             } else {
